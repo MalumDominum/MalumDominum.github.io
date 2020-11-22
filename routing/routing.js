f833f28736f1a8dc98046964149d0aacdfd6f443
Mihail Alekseev <mihailalekseev02@gmail.com>
2020-11-22 21:49:44 +0200

    JS restructuring

---
+++ routing/routing.js
@@ -0,0 +1,22 @@
+'use strict';
+import { pageWidth } from 'main.js';
+console.log(pageWidth)
+// const router = document.getElementById('router');
+
+// function routeLogic(routeElement) {
+//     history.pushState(null, null, routeElement.dataset.route);
+//     OnRoute();
+// };
+
+// function initiateRoutes() {
+//   let routeElements = document.querySelectorAll('[data-route]');
+//   routeElements.forEach(function(routeElement) {
+//     routeElement.addEventListener('click', routeLogic(routeElement))
+//   });
+// };
+
+// initiateRoutes();
+
+// function OnRoute() {
+//   router.innerHTML = "";
+// }
