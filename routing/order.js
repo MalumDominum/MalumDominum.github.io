0b579a3a38fa5cc3e090d9f4f80ce7cecdea6783
Mihail Alekseev <mihailalekseev02@gmail.com>
2020-12-07 00:38:40 +0200

    Delete sticker

---
+++ routing/order.js
@@ -0,0 +1,9 @@
+'use strict';
+const dbUrl = "https://my-json-server.typicode.com/MalumDominum/MalumDominum.github.io";
+
+
+let constructor = async function(container) {
+
+};
+
+export { constructor };
